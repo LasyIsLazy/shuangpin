8b882b0d6484986391f1cecfb0541d1ac397f395
lsy <dash0forward@gmail.com>
2019-01-28 17:47:52 +0800

    增加练习方案的功能

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@
         :value="item.value"
       ></el-option>
     </el-select>
+    <label for="keyPlan">练习方案</label>
+    <el-select v-model="keyPlan" name="keyPlan" @change="handleKeyPlanChange">
+      <el-option
+        v-for="(item, index) in KEY_PLANS"
+        :key="index"
+        :label="item.name"
+        :value="item.keys"
+      ></el-option>
+    </el-select>
     <div class="input-container">
       <el-row>
         <el-col :span="12">
@@ -51,6 +60,8 @@ import ShuangpinKeys from './components/ShuangpinKeys'
 import { plans } from './commons/config'
 import ALL_PINYIN from './commons/pinyin'
 import SHUANGPIN from './commons/shuangpin'
+import KEY_PLANS from './commons/key-plan.js'
+import SHUANGPIN_KEYS from './commons/shuangpin-key.js'
 
 export default {
   name: 'App',
@@ -61,6 +72,8 @@ export default {
     return {
       plans,
       plan: '', // 双拼方案
+      KEY_PLANS, // 所有练习方案
+      keyPlan: null, // 练习方案（键位数组）
       input: '',
       initial: '',
       final: '',
@@ -76,11 +89,72 @@ export default {
     },
     pinyin () {
       return this.initial + this.final
+    },
+    /**
+     * 当前方案的双拼对应键位
+     */
+    shuangpinKeys () {
+      return SHUANGPIN_KEYS[this.plan]
+    },
+    /**
+     * 当前方案的双拼所有声母
+     */
+    shuangpinInitials () {
+      let result = []
+      this.keyPlan.forEach(key => {
+        const { initials } = this.shuangpinKeys[key] // 该键位对应的声母
+        result = result.concat(initials)
+      })
+      return result
+    },
+    /**
+     * 当前方案的双拼所有韵母
+     */
+    shuangpinFinals () {
+      let result = []
+      this.keyPlan.forEach(key => {
+        const { finals } = this.shuangpinKeys[key] // 该键位对应的声母
+        result = result.concat(finals)
+      })
+      return result
+    },
+    /**
+     * 当前方案对应的所有拼音
+     */
+    pinyins () {
+      if (!this.keyPlan) {
+        return ALL_PINYIN
+      }
+      // 过滤不在当前方案的拼音
+      const result = ALL_PINYIN
+        // 过滤掉声母不在当前方案的拼音
+        .filter(pinyin => {
+          const { initial } = pinyin
+          if (this.shuangpinInitials.findIndex(ele => initial === ele) === -1 && this.keyPlan.findIndex(ele => ele === initial)) {
+            // 声母不在当前方案中 => 过滤掉
+            return false
+          }
+          return true
+        }, this)
+        // 过滤不在练习方案的韵母
+        .map(pinyin => {
+          let returnPinyin = pinyin
+          const { finals } = pinyin
+          const filteredFinals = finals.filter(final => this.shuangpinFinals.findIndex(ele => ele === final) !== -1) // 过滤掉拼音的韵母不在练习方案韵母中的
+          returnPinyin.finals = filteredFinals
+          return returnPinyin
+        })
+        // 过滤没有韵母的
+        .filter(pinyin => pinyin.finals.length !== 0)
+      return result
     }
   },
   methods: {
+    /**
+     * 下一个
+     */
     next () {
-      const pinyin = ALL_PINYIN[Math.floor(Math.random() * ALL_PINYIN.length)]
+      const pinyin = this.pinyins[Math.floor(Math.random() * this.pinyins.length)]
       const { initial, finals } = pinyin
       const final = finals[Math.floor(Math.random() * finals.length)]
       this.initial = initial
@@ -99,6 +173,12 @@ export default {
     },
     handleTab () {
       this.showPrompt()
+    },
+    /**
+     * 练习方案改变
+     */
+    handleKeyPlanChange () {
+      this.next()
     },
     validate () {
       return new Promise((resolve, reject) => {
---
+++ src/commons/key-plan.js
@@ -0,0 +1,10 @@
+/**
+ * 键位联系方案
+ */
+
+export default [
+  { name: 'QWASZX', keys: ['q', 'w', 'a', 's', 'z', 'x'] },
+  { name: 'ERDFCV', keys: ['e', 'r', 'd', 'f', 'c', 'v'] },
+  { name: 'TYGHBN', keys: ['t', 'y', 'g', 'h', 'b', 'n'] },
+  { name: 'UIOPJKLM', keys: ['u', 'i', 'o', 'p', 'j', 'k', 'l', 'm'] }
+]
